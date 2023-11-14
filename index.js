5ba4cc466f3677a49622ceed53b216130ab40824
shuks91 <arteh@live.co.uk>
2023-11-14 16:22:44 +0000

    puzzle3 added

--- index.js
+++ index.js
@@ -1,39 +1,83 @@
 import inquirer from "inquirer";
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"]; //choices tha are avaliable
-  const randomIndex = Math.floor(Math.random() * choices.length); // random number to fit the lenght of the choices array
-  return choices[randomIndex];
+
+// move the below to own file to separate!! ------------------
+
+
+
+const puzzle3 = async () => {
+  let { result } = await inquirer.prompt({
+    type: "list",
+    name: "result",
+    message: "20 % of 2 is equal to?",
+    choices: ["20", "4", "0.4", "0.004"]
+  })
+  return result
 }
 
-function determineWinner(userChoice, computerChoice) {
-  if (userChoice === computerChoice) {
-    return "Its a tie";
-  } else if (
-    (userChoice === "rock" && computerChoice === "scissor") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
-    return "You win!";
+const displayPuzzle3 = async () => {
+  let rightAnswer = "0.4"
+  let userAnswer = await puzzle3();
+  console.log('ANSWERRR', userAnswer, rightAnswer)
+  if (userAnswer === rightAnswer) {
+    console.log("CORRECT! You are free to move on")
   } else {
-    return "You lose!";
+    console.log("INCORRECT. You're stuck!")
   }
 }
 
-function playGame() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "userChoice",
-        message: "Your move",
-        choices: ["rock", "paper", "scissor"],
-      },
-    ])
-    .then((answers) => {
-      const userChoice = answers.userChoice;
-      const computerChoice = getComputerChoice();
-      console.log(`Computer chose: ${computerChoice}`);
-      console.log(determineWinner(userChoice, computerChoice));
-    });
+// -----------------------------------------
+
+// const puzzle4 = async () => {
+
+// }
+
+// const displayPuzzle4 = async () => {
+
+// }
+// ----------------------------------------
+
+class EscapeRoomGame {
+  constructor() {
+  }
+
+  welcome() {
+    console.log("---------------------------------")
+    console.log("Welcome to our escape room. You're trapped. Solve the problems to make your way out. Warning - you may be timed!");
+    console.log("---------------------------------")
+  }
+
+  playPuzzle3() {
+    displayPuzzle3()
+  }
+
+  // playPuzzle4() {
+  //   displayPuzzle4()
+  // }
+
 }
-playGame();
+
+
+
+
+
+// const level = async () => {
+//   let { answer } = await inquirer.prompt({
+//     name: "result",
+//     type: "list",
+//     message: "Choose difficulty level?",
+//     choices: ["easy", "medium", "hard"]
+
+//   })
+
+// return answer
+// }
+
+// const display = async () => {
+//   let response = await level()
+//   console.log(response)
+// }
+
+const game = new EscapeRoomGame()
+game.welcome();
+// game.level();
+game.playPuzzle3();
