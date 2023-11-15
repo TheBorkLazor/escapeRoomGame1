6ed2163c409921d29d5bfd0626ee344998b9b718
Andrew Smith <Andrewwilliamjohnsmith@hotmail.co.uk>
2023-11-15 10:11:04 +0000

    Update 2 Working Game

--- index.js
+++ index.js
@@ -63,8 +63,66 @@ function searchRoom() {
     });
 }
 
+function mathsPuzzle() {
+  return inquirer
+    .prompt({
+      type: "list",
+      name: "result",
+      message: "20% of 2 is equal to?",
+      choices: ["20", "4", "0.4", "0.004"],
+    })
+    .then((result) => {
+      let rightAnswer = "0.4";
+      let userAnswer = result.result;
+      console.log("ANSWER", userAnswer, rightAnswer);
+      if (userAnswer === rightAnswer) {
+        console.log("CORRECT! You are free to move on");
+      } else {
+        console.log("INCORRECT. You're stuck!");
+      }
+    });
+}
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"]; //choices tha are avaliable
+  const randomIndex = Math.floor(Math.random() * choices.length); // random number to fit the lenght of the choices array
+  return choices[randomIndex];
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "Its a tie";
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissor") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
+
+function playGame() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "userChoice",
+        message: "Your move",
+        choices: ["rock", "paper", "scissor"],
+      },
+    ])
+    .then((answers) => {
+      const userChoice = answers.userChoice;
+      const computerChoice = getComputerChoice();
+      console.log(`Computer chose: ${computerChoice}`);
+      console.log(determineWinner(userChoice, computerChoice));
+    });
+}
+
 async function startGame() {
-  console.log("Welcome to the Excape Room Game!");
+  console.log("Welcome to the Escape Room Game!");
 
   const username = await getName();
   console.log(`Hello, ${username}! Let's begin.`);
@@ -73,45 +131,11 @@ async function startGame() {
   room1.enter();
 
   await searchRoom();
+
+  await mathsPuzzle();
+
+  console.log("Now, lets play a game of Rock, Paper, Scissors!");
+  await playGame();
 }
 
 startGame();
-
-// function getComputerChoice() {
-//   const choices = ["rock", "paper", "scissors"]; //choices tha are avaliable
-//   const randomIndex = Math.floor(Math.random() * choices.length); // random number to fit the lenght of the choices array
-//   return choices[randomIndex];
-// }
-
-// function determineWinner(userChoice, computerChoice) {
-//   if (userChoice === computerChoice) {
-//     return "Its a tie";
-//   } else if (
-//     (userChoice === "rock" && computerChoice === "scissor") ||
-//     (userChoice === "paper" && computerChoice === "rock") ||
-//     (userChoice === "scissors" && computerChoice === "paper")
-//   ) {
-//     return "You win!";
-//   } else {
-//     return "You lose!";
-//   }
-// }
-
-// function playGame() {
-//   inquirer
-//     .prompt([
-//       {
-//         type: "list",
-//         name: "userChoice",
-//         message: "Your move",
-//         choices: ["rock", "paper", "scissor"],
-//       },
-//     ])
-//     .then((answers) => {
-//       const userChoice = answers.userChoice;
-//       const computerChoice = getComputerChoice();
-//       console.log(`Computer chose: ${computerChoice}`);
-//       console.log(determineWinner(userChoice, computerChoice));
-//     });
-// }
-playGame();
